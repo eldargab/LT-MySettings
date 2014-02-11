722e0f65eab5a2c4cf0e0d8f885a2bd54acf035e
Eldar Gabdullin <eldargab@gmail.com>
2014-02-11 14:33:53 +0400

    fix indentation

--- codemirror/clojure-mode.js
+++ codemirror/clojure-mode.js
@@ -22,7 +22,7 @@ CodeMirror.defineMode("clojure", function () {
     "ns fn def defn defmethod bound-fn if if-not case condp when while when-not when-first do future comment doto locking proxy with-open with-precision reify deftype defrecord defprotocol extend extend-protocol extend-type try catch " +
 
     // ClojureScript
-    "this-as" +
+    "this-as " +
 
     // Binding forms
     "let letfn binding loop for doseq dotimes when-let if-let " +
@@ -40,15 +40,15 @@ CodeMirror.defineMode("clojure", function () {
     "handler-case handle dotrace deftrace" +
 
     // HTML tags
-    ("a abbr address area article aside audio b base bdi bdo big blockquote body" +
-    "br button canvas caption cite code col colgroup data datalist dd del details" +
-    "dfn div dl dt em embed fieldset figcaption figure footer form h1 h2 h3 h4 h5 h6" +
-    "head header hr html i iframe img input ins kbd keygen label legend li link main" +
-    "map mark menu menuitem meta meter nav noscript object ol optgroup option output" +
-    "p param pre progress q rp rt ruby s samp script section select small source" +
-    "span strong style sub summary sup table tbody td textarea tfoot th thead time" +
-    "title tr track u ul var video wbr" +
-    "circle g line path polygon polyline rect svg text").toUpperCase()
+    ("a abbr address area article aside audio b base bdi bdo big blockquote body " +
+    "br button canvas caption cite code col colgroup data datalist dd del details " +
+    "dfn div dl dt em embed fieldset figcaption figure footer form h1 h2 h3 h4 h5 h6 " +
+    "head header hr html i iframe img input ins kbd keygen label legend li link main " +
+    "map mark menu menuitem meta meter nav noscript object ol optgroup option output " +
+    "p param pre progress q rp rt ruby s samp script section select small source " +
+    "span strong style sub summary sup table tbody td textarea tfoot th thead time " +
+    "title tr track u ul var video wbr " +
+    "circle g line path polygon polyline rect svg text ").toUpperCase()
   );
 
   var opposites = {")": "(",
